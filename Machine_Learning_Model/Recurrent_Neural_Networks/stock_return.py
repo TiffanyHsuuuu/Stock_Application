2251a042566b35dd8deea01e04ab01cafe38fa5d
Tiffi <tiffany.hsu2009@gmail.com>
2020-04-18 09:52:34 +0200

    update

---
+++ Machine_Learning_Model/Recurrent_Neural_Networks/stock_return.py
@@ -0,0 +1,65 @@
+
+from tensorflow.keras.layers import Input, LSTM, GRU, SimpleRNN, Dense, GlobalMaxPool1D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+
+df = pd.read_csv('AAPL_data.csv', sep=r'\t', engine='python')
+
+series = df['4. close'].values.reshape(-1,1)
+
+# Normalize the data
+scaler = StandardScaler()
+scaler.fit(series[:len(series)//2])
+series = scaler.transform(series).flatten()
+
+# Build dataset
+T = 10 #use T past values to predict the next value
+D = 1
+X = []
+Y = []
+for t in range(len(series) - T):
+    x = series[t:t+T]
+    X.append(x)
+    y = series[t+T]
+    Y.append(y)
+X = np.array(X).reshape(-1, T, 1) #data shall be N*T*D
+Y = np.array(Y)
+N = len(X)
+print("X.shape", X.shape, "Y.shape", Y.shape)
+
+# try autoregressive RNN model
+i = Input(shape=(T,1))
+x = LSTM(5)(i)
+x = Dense(1)(x)
+model = Model(i,x)
+model.compile(
+    loss = 'mse',
+    optimizer = Adam(lr=0.1),
+)
+
+# train RNN
+r = model.fit(
+    X[:-N//2], Y[:-N//2],
+    epochs=80,
+    validation_data=(X[-N//2:], Y[-N//2:]),
+)
+
+# plot loss per iteration
+plt.plot(r.history['loss'], label='loss from test data')
+plt.plot(r.history['val_loss'], label='loss from training data')
+plt.legend()
+plt.show()
+
+# one-step forecast using true targets
+predict = model.predict(X)
+predictions = predict[:,0]
+plt.plot(Y, label='Targets')
+plt.plot(predictions, label='Predictions')
+plt.legend()
+plt.show()
--- Python_Framework/app/app.py
+++ Python_Framework/app/app.py
@@ -1,8 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from flask_jwt import JWT
-from flask import render_template
 
 from security import authenticate, identity
 from resources.user_controller import UserController
@@ -18,17 +17,11 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(UserController, '/register')
-api.add_resource(StockController, '/stock/<string:name>')
-api.add_resource(StockList, '/stocks')
-
 db.init_app(app)
-
 
 @app.before_first_request
 def create_tables():
     db.create_all()
-
 
 @app.route('/')
 def frontpage():
